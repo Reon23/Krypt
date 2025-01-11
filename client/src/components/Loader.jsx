204b1f162a367ff80472b7795828aa9c2e4169d0
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-01-11 10:38:23 +0530

    Welcome Section

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,9 @@
 
 const Loader = () => {
     return (
-        <h1>Loader</h1>
+        <div className="flex justify-center items-center py-3">
+            <div className="animate-spin rounded-full w-32 h-32 border-b-2 border-red-700" />
+        </div>
     );
 }
 
