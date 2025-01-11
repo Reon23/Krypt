b529bd8ecc2b816e074ba743869ba2d3f0bbafd7
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-01-11 09:24:51 +0530

    Welcome Section

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,4 @@
-export { default as loader } from './Loader';
+export { default as Loader } from './Loader';
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Welcome } from './Welcome';
