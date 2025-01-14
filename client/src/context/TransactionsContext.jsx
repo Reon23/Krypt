f335d94ada6ec4a9b584b8c133971050a6191e13
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-01-14 14:21:27 +0530

    handle transaction

--- client/src/context/TransactionsContext.jsx
+++ client/src/context/TransactionsContext.jsx
@@ -8,21 +8,18 @@ export const TransactionContext = React.createContext();
 const { ethereum } = window;
 
 const getEthereumContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
+    const provider = new ethers.BrowserProvider(ethereum);
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log({
-        provider,
-        signer,
-        transactionContract
-
-    });
+    return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState('');
     const [formData, setFormData ] = useState({ addressTo: '', amount: '', keyword: '', message: ''});
+    const [isLoading, setIsLoading ] = useState(false);
+    const [transactionCount, setTransactionCount ] = useState(localStorage.getItem('transactionCount'));
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
@@ -65,6 +62,32 @@ export const TransactionProvider = ({ children }) => {
     const sendTransaction = async () => {
         try {
             if (!ethereum) return alert("Please install metamask");
+            const { addressTo, amount, keyword, message } = formData;
+            const transactionContract = getEthereumContract();
+            const parsedAmount = ethers.parseEther(amount);
+
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: '0x5208',
+                    value: parsedAmount._hex,
+                }]
+            });
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount;
+
+            setTransactionCount(transactionCount.toNumber())
+
         } catch (error) {
             console.log(error);
 
@@ -77,7 +100,7 @@ export const TransactionProvider = ({ children }) => {
     }, []);
 
     return (
-        <TransactionContext.Provider value={{ connectWallet , currentAccount, formData, setFormData, handleChange }}>
+        <TransactionContext.Provider value={{ connectWallet , currentAccount, formData, setFormData, handleChange, sendTransaction }}>
             { children }
         </TransactionContext.Provider>
     )
