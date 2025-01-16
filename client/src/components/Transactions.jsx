f9a0cdd0eaf30faef00583013c1bd5fbe1b102e1
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-01-16 15:23:15 +0530

    Transaction History

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,8 +3,10 @@ import { TransactionContext } from '../context/TransactionsContext';
 
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
+import useFetch from '../hooks/useFetch';
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+    const gifUrl = useFetch({ keyword })
     return (
         <div className='bg-[#181918] m-4 flex flex-1 
         2xl:min-w-[450px] 
@@ -31,13 +33,11 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                         </>
                     )}
 
-
-
+                </div>
+                    <img src={gifUrl || url} alt="gif" className='w-full h-64 2xl h-96 rounded-md shadow-lg object-cover' />
                     <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'>
                         <p className='text-[#37c7da] font-bold'>{timestamp}</p>
                     </div>
-
-                </div>
             </div>
         </div>
     )
